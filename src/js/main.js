f5a6cc950e7af483cb089176714959cc31d5770a
Hellish <oliinyk0590@gmail.com>
2019-03-04 21:20:26 +0200

    Добавил слайдер и выровнял макет под Pixel

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,24 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
-
-button.addEventListener('click', function() {
-  modal.classList.add('modal_active');
+$(document).ready(function () {
+  $('body').append('<a href="#" id="go-top" title="Вверх"><img src="img/arrow-up.png" alt="Вверх"></a>');
 });
 
-close.addEventListener('click', function () {
-  modal.classList.remove('modal_active');
+$(function () {
+  $.fn.scrollToTop = function () {
+    $(this).hide().removeAttr("href");
+    if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+    var scrollDiv = $(this);
+    $(window).scroll(function () {
+      if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+      else $(scrollDiv).fadeIn("slow")
+    });
+    $(this).click(function () {
+      $("html, body").animate({
+        scrollTop: 0
+      }, "slow")
+    })
+  }
 });
 
+$(function () {
+  $("#go-top").scrollToTop();
+});
